a4d8eb1d228bb2fb2f4ce9e7b304397bc5855b43
Will Tyler <will.tyler11@gmail.com>
2018-06-18 13:49:04 -0400

    Initial Commit

---
+++ GroupMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GroupMac
+//
+//  Created by Will Tyler on 6/18/18.
+//  Copyright © 2018 Will Tyler. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ GroupMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GroupMac
+//
+//  Created by Will Tyler on 6/18/18.
+//  Copyright © 2018 Will Tyler. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
