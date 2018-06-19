5ac413049bad83c6b055f5cc230c7d5d249e999b
Will Tyler <will.tyler11@gmail.com>
2018-06-19 19:45:24 -0400

    Add synchronous request method, and get GroupMe.Group.groups working. Inits that are not needed will be removed

--- GroupMac/GroupMe/GroupMe.swift
+++ GroupMac/GroupMe/GroupMe.swift
@@ -16,40 +16,164 @@ class GroupMe {
 		let path = Bundle.main.path(forResource: "token", ofType: "txt")!
 		let token = try! String(contentsOfFile: path).trimmingCharacters(in: .whitespacesAndNewlines)
 
+		print("Access Token: \"\(token)\"")
+
 		return token
 	}()
 
-	static var groups: [String] {
-		get {
-			let url = URL(string: baseURL.absoluteString + "/groups?token=" + accessToken)!
-			let request: URLRequest = {
-				var request = URLRequest(url: url)
-				request.httpMethod = "GET"
+	class Group: Decodable {
 
-				return request
-			}()
+		let id: String
+		let name: String
+		let phoneNumber: String
+		let type: String
+		let description: String
+		let imageURL: URL?
+		let creatorUserID: String
+		let createdAt: Int
+		let updatedAt: Int
+		let officeMode: Bool
+		let shareURL: URL?
+		let shareQRCodeURL: URL?
+		let members: [Member]
+//		let messageInfo: MessageInfo?
+		let maxMembers: Int
 
-			let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-				guard error == nil else {
-					print(error!)
+//		init(id: String, name: String, phoneNumber: String, type: String, description: String, imageURL: URL?, creatorUserID: String, createdAt: Int, updatedAt: Int, officeMode: Bool, shareURL: URL?, shareQRCodeURL: URL?, members: [Member],/* messageInfo: MessageInfo?,*/ maxMembers: Int) {
+//			self.id = id
+//			self.name = name
+//			self.phoneNumber = phoneNumber
+//			self.type = type
+//			self.description = description
+//			self.imageURL = imageURL
+//			self.creatorUserID = creatorUserID
+//			self.createdAt = createdAt
+//			self.updatedAt = updatedAt
+//			self.officeMode = officeMode
+//			self.shareURL = shareURL
+//			self.shareQRCodeURL = shareQRCodeURL
+//			self.members = members
+//			self.messageInfo = messageInfo
+//			self.maxMembers = maxMembers
+//		}
 
-					return
-				}
-
-				guard let data = data else {
-					fatalError("No data.")
-				}
-
-				let dataDict = try! JSONSerialization.jsonObject(with: data) as! [String: Any]
-
-				for item in dataDict {
-					print(item)
-				}
-			}
-			task.resume()
-
-			return [String]()
+		enum CodingKeys: String, CodingKey {
+			case id
+			case name
+			case phoneNumber = "phone_number"
+			case type
+			case description
+			case imageURL = "image_url"
+			case creatorUserID = "creator_user_id"
+			case createdAt = "created_at"
+			case updatedAt = "updated_at"
+			case officeMode = "office_mode"
+			case shareURL = "share_url"
+			case shareQRCodeURL = "share_qr_code_url"
+			case members
+//			case messageInfo = "messages"
+			case maxMembers = "max_members"
 		}
+
+//		class PreviewMessage: Decodable {
+//			let nickname: String
+//			let text: String
+//			let imageURL: URL?
+//
+//			init(nickname: String, text: String, imageURL: URL?) {
+//				self.nickname = nickname
+//				self.text = text
+//				self.imageURL = imageURL
+//			}
+//
+//			enum CodingKeys: String, CodingKey {
+//				case nickname
+//				case text
+//				case imageURL = "image_url"
+//			}
+//		}
+//
+//		class MessageInfo: Decodable {
+//			let count: Int
+//			let lastMessageID: String
+//			let lastMessageCreatedAt: Int
+//			let preview: PreviewMessage?
+//
+//			init(count: Int, lastMessageID: String, lastMessageCreatedAt: Int, preview: PreviewMessage?) {
+//				self.count = count
+//				self.lastMessageID = lastMessageID
+//				self.lastMessageCreatedAt = lastMessageCreatedAt
+//				self.preview = preview
+//			}
+//
+//			enum CodingKeys: String, CodingKey {
+//				case count
+//				case lastMessageID = "last_message_id"
+//				case lastMessageCreatedAt = "last_message_created_at"
+//				case preview
+//			}
+//		}
+
+		class Member: Decodable {
+			let userID: String
+			let nickname: String
+			let imageURL: URL?
+			let id: String
+			let isMuted: Bool
+			let isAutokicked: Bool
+			let roles: [String]
+
+//			init(userID: String, nickname: String, imageURL: URL?, id: String, isMuted: Bool, isAutokicked: Bool, roles: [String]) {
+//				self.userID = userID
+//				self.nickname = nickname
+//				self.imageURL = imageURL
+//				self.id = id
+//				self.isMuted = isMuted
+//				self.isAutokicked = isAutokicked
+//				self.roles = roles
+//			}
+
+			enum CodingKeys: String, CodingKey {
+				case userID = "user_id"
+				case nickname
+				case imageURL = "image_url"
+				case id
+				case isMuted = "muted"
+				case isAutokicked = "autokicked"
+				case roles
+			}
+		}
+
+		static var groups: [Group] {
+			get {
+				let url = URL(string: baseURL.absoluteString + "/groups?token=" + accessToken)!
+				let request: URLRequest = {
+					var request = URLRequest(url: url)
+					request.httpMethod = HTTPRequestMethod.get.rawValue
+
+					return request
+				}()
+
+				let results: HTTP.Response = HTTP.syncRequest(request: request)
+
+				guard results.error == nil else {
+					print(results.error!)
+
+					return [Group]()
+				}
+
+				let json = try! JSONSerialization.jsonObject(with: results.data!) as! [String: Any]
+
+				let groups: [Group] = {
+					let data = try! JSONSerialization.data(withJSONObject: json["response"]!)
+
+					return try! JSONDecoder().decode([Group].self, from: data)
+				}()
+
+				return groups
+			}
+		}
+
 	}
 
 }
