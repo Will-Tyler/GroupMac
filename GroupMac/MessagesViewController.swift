f5e8f2571fe44b0b0c0ff0a135f1659aeb5463cb
Will Tyler <will.tyler11@gmail.com>
2018-07-02 20:00:54 -0400

    Add avatar image to display in conversations

--- GroupMac/MessagesViewController.swift
+++ GroupMac/MessagesViewController.swift
@@ -73,7 +73,7 @@ class MessagesViewController: NSViewController, NSCollectionViewDelegateFlowLayo
 		// Creating a cell doesn't work. Recreate labels to get estimated desired height
 
 		let desiredHeight: CGFloat = {
-			let restrictedSize = CGSize(width: collectionView.bounds.width-8, height: .greatestFiniteMagnitude)
+			let restrictedSize = CGSize(width: collectionView.bounds.width - (12+24), height: .greatestFiniteMagnitude)
 			let drawingOptions = NSString.DrawingOptions.usesFontLeading.union(.usesLineFragmentOrigin)
 			let labels: (name: NSString, text: NSString?) = {
 				let count = messages!.count
@@ -85,7 +85,7 @@ class MessagesViewController: NSViewController, NSCollectionViewDelegateFlowLayo
 			let nameEstimate: CGRect = labels.name.boundingRect(with: restrictedSize, options: drawingOptions, attributes: [.font: Fonts.boldSmall])
 			let textEstimate: CGRect? = labels.text?.boundingRect(with: restrictedSize, options: drawingOptions, attributes: [.font: Fonts.regular])
 
-			return nameEstimate.height + (textEstimate?.height ?? 0)
+			return nameEstimate.height + (textEstimate?.height ?? 0) + 1
 		}()
 
 		return NSSize(width: collectionView.bounds.width, height: desiredHeight)
@@ -95,6 +95,17 @@ class MessagesViewController: NSViewController, NSCollectionViewDelegateFlowLayo
 
 final fileprivate class MessageCell: NSCollectionViewItem {
 	static let cellIdentifier = NSUserInterfaceItemIdentifier(rawValue: "MessageCell")
+	private let avatarImageView: NSImageView = {
+		let view = NSImageView()
+
+		view.image = #imageLiteral(resourceName: "Person Default Image")
+		view.imageScaling = NSImageScaling.scaleAxesIndependently
+		view.wantsLayer = true
+		view.layer!.cornerRadius = 12
+		view.layer!.masksToBounds = true
+
+		return view
+	}()
 	private let nameLabel: NSTextField = {
 		let field = NSTextField()
 
@@ -121,15 +132,34 @@ final fileprivate class MessageCell: NSCollectionViewItem {
 			if let text = message.text {
 				textLabel.stringValue = text
 			}
+
+			if let url = message.avatarURL {
+				URLSession.shared.dataTask(with: url) { (data: Data?, response: URLResponse?, error: Error?) in
+					guard error == nil, let data = data else { return }
+
+					let image = NSImage(data: data) ?? #imageLiteral(resourceName: "Person Default Image")
+					DispatchQueue.main.async {
+						self.avatarImageView.image = image
+					}
+				}.resume()
+			}
 		}
 	}
 
 	private func setupInitialLayout() {
+		view.addSubview(avatarImageView)
+
+		avatarImageView.translatesAutoresizingMaskIntoConstraints = false
+		avatarImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+		avatarImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 4).isActive = true
+		avatarImageView.heightAnchor.constraint(equalToConstant: 24).isActive = true
+		avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor).isActive = true
+
 		view.addSubview(nameLabel)
 
 		nameLabel.translatesAutoresizingMaskIntoConstraints = false
 		nameLabel.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-		nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+		nameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 4).isActive = true
 		nameLabel.heightAnchor.constraint(equalToConstant: nameLabel.intrinsicContentSize.height).isActive = true
 		nameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
 
@@ -137,7 +167,7 @@ final fileprivate class MessageCell: NSCollectionViewItem {
 
 		textLabel.translatesAutoresizingMaskIntoConstraints = false
 		textLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor).isActive = true
-		textLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+		textLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 4).isActive = true
 		textLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
 	}
 
@@ -146,7 +176,6 @@ final fileprivate class MessageCell: NSCollectionViewItem {
 			let view = NSView()
 
 			view.wantsLayer = true
-//			view.layer?.backgroundColor = CGColor(red: 0xe5 / 255, green: 0xf1 / 255, blue: 0xf6 / 255, alpha: 1)
 			view.layer?.backgroundColor = NSColor.systemPink.cgColor
 
 			return view
