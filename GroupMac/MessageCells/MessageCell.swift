27124b1c0fd7ef91537c2cc2bfb06abb3a7d5ad6
Will Tyler <will.tyler11@gmail.com>
2018-07-04 17:20:22 -0400

    Work on supporting system messages by adding a new cell, place cell source code in separate files

---
+++ GroupMac/MessageCells/MessageCell.swift
@@ -0,0 +1,113 @@
+//
+//  MessageCell.swift
+//  GroupMac
+//
+//  Created by Will Tyler on 7/4/18.
+//  Copyright © 2018 Will Tyler. All rights reserved.
+//
+
+import Cocoa
+
+
+final class MessageCell: NSCollectionViewItem {
+
+	static let cellIdentifier = NSUserInterfaceItemIdentifier(rawValue: "MessageCell")
+	private let avatarImageView: NSImageView = {
+		let view = NSImageView()
+
+		view.image = #imageLiteral(resourceName: "Person Default Image")
+		view.imageScaling = NSImageScaling.scaleAxesIndependently
+		view.wantsLayer = true
+		view.layer!.cornerRadius = 15
+		view.layer!.masksToBounds = true
+
+		return view
+	}()
+	private let nameLabel: NSTextField = {
+		let field = NSTextField()
+
+		field.isEditable = false
+		field.isBezeled = false
+		field.font = Fonts.boldSmall
+		field.textColor = NSColor(red: 0x62 / 255, green: 0x6f / 255, blue: 0x82 / 255, alpha: 1)
+		field.backgroundColor = .clear
+
+		return field
+	}()
+	private let textLabel: NSTextField = {
+		let field = NSTextField()
+
+		field.isEditable = false
+		field.isBezeled = false
+		field.font = Fonts.regular
+		field.backgroundColor = NSColor.clear
+
+		return field
+	}()
+	private var importantAvatarConstraint: NSLayoutConstraint!
+
+	var message: GMMessage! {
+		didSet {
+			nameLabel.stringValue = message.name
+			if let text = message.text {
+				textLabel.stringValue = text
+			}
+
+			if message.isSystem {
+				//				DispatchQueue.main.async {
+				//					self.convertToSystemMessageLayout()
+				//				}
+			}
+			else {
+				HTTP.handleImage(at: message.avatarURL, with: { (image: NSImage) in
+					DispatchQueue.main.async {
+						self.avatarImageView.image = image
+					}
+				})
+			}
+		}
+	}
+
+	private func setupInitialLayout() {
+		view.addSubview(avatarImageView)
+
+		avatarImageView.translatesAutoresizingMaskIntoConstraints = false
+		importantAvatarConstraint = avatarImageView.topAnchor.constraint(equalTo: view.topAnchor)
+		importantAvatarConstraint.isActive = true
+		avatarImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 4).isActive = true
+		avatarImageView.heightAnchor.constraint(equalToConstant: 30).isActive = true
+		avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor).isActive = true
+
+		view.addSubview(nameLabel)
+
+		nameLabel.translatesAutoresizingMaskIntoConstraints = false
+		nameLabel.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+		nameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 4).isActive = true
+		nameLabel.heightAnchor.constraint(equalToConstant: nameLabel.intrinsicContentSize.height).isActive = true
+		nameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+
+		view.addSubview(textLabel)
+
+		textLabel.translatesAutoresizingMaskIntoConstraints = false
+		textLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor).isActive = true
+		textLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 4).isActive = true
+		textLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+	}
+
+	override func loadView() {
+		view = {
+			let view = NSView()
+
+			view.wantsLayer = true
+			view.layer!.backgroundColor = Colors.background
+
+			return view
+		}()
+	}
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		setupInitialLayout()
+	}
+
+}
