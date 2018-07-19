9c49edc2b0a2586e0f087b812627f5b99b46a06a
Will Tyler <will.tyler11@gmail.com>
2018-07-19 14:11:58 -0400

    Improve window style mask code

--- GroupMac/AppDelegate.swift
+++ GroupMac/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 		window.setFrame(NSRect(x: window.screen!.visibleFrame.midX, y: window.screen!.visibleFrame.midY, width: 500, height: 300), display: true)
 		window.center()
-		window.styleMask = NSWindow.StyleMask(rawValue: NSWindow.StyleMask.RawValue((UInt8(NSWindow.StyleMask.titled.rawValue) | UInt8(NSWindow.StyleMask.closable.rawValue) | UInt8(NSWindow.StyleMask.miniaturizable.rawValue) | UInt8(NSWindow.StyleMask.resizable.rawValue))))
+		window.styleMask = [.miniaturizable, .closable, .resizable, .titled]
 		window.title = "GroupMac"
 		window.isMovable = true
 		window.minSize = NSSize(width: 500, height: 300)
@@ -36,10 +36,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 		// Insert code here to initialize your application
 
 		windowController.window!.makeKeyAndOrderFront(nil)
-	}
-
-	func applicationWillTerminate(_ aNotification: Notification) {
-		// Insert code here to tear down your application
 	}
 
 }
