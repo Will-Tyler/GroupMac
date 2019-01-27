f78ff9eb5aefae0c4abe627cc002d6893da10a80
Will Tyler <will.tyler11@gmail.com>
2019-01-27 21:40:42 +0000

    Successfully save window frame when size changed or moved, and load on launch

--- GroupMac/AppDelegate.swift
+++ GroupMac/AppDelegate.swift
@@ -9,27 +9,52 @@
 import AppKit
 
 
-final class AppDelegate: NSObject, NSApplicationDelegate {
+final class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
+
+	private var window: NSWindow!
+	private let defaults = UserDefaults.standard
+	private let windowFrameKey = "window.frame"
 
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
-		let window = NSWindow()
+		window = NSWindow()
+
 		let screen = window.screen!
 		let screenSize = screen.size
+		let windowFrame: NSRect
 
-		let initialWindowFrame = NSRect(x: screen.visibleFrame.midX, y: screen.visibleFrame.midY, width: screenSize.width / 4, height: screenSize.height / 4)
+		if let frameData = defaults.object(forKey: windowFrameKey) as? Data, let frame = NSKeyedUnarchiver.unarchiveObject(with: frameData) as? NSRect {
+			windowFrame = frame
+		}
+		else {
+			windowFrame = NSRect(x: screen.visibleFrame.midX, y: screen.visibleFrame.midY, width: screenSize.width / 4, height: screenSize.height / 4)
+		}
 
+		window.delegate = self
 		window.minSize = NSSize(width: 500, height: 300)
-		window.setFrame(initialWindowFrame, display: true)
-		window.center()
 		window.styleMask = [.miniaturizable, .closable, .resizable, .titled]
 		window.title = "GroupMac"
 		window.isMovable = true
 		window.contentViewController = ViewController()
+		window.setFrame(windowFrame, display: false)
 
 		let windowController = NSWindowController(window: window)
 
 		windowController.shouldCascadeWindows = true
-		windowController.window!.makeKeyAndOrderFront(self)
+		window.makeKeyAndOrderFront(self)
+	}
+
+	private func saveWindowFrame() {
+		let frame = window.frame
+		let frameData = NSKeyedArchiver.archivedData(withRootObject: frame)
+
+		defaults.set(frameData, forKey: windowFrameKey)
+	}
+
+	func windowDidEndLiveResize(_ notification: Notification) {
+		saveWindowFrame()
+	}
+	func windowDidMove(_ notification: Notification) {
+		saveWindowFrame()
 	}
 
 }
